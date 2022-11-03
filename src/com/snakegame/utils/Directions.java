6f1de96043e30e4698e5d2c9a02a7983ed2dee99
R4JxF60 <rajithakumaraprog@gmail.com>
2022-11-03 09:49:40 +0530

    add: directions class

---
+++ src/com/snakegame/utils/Directions.java
@@ -0,0 +1,12 @@
+package com.snakegame.utils;
+
+public class Directions {
+	final public static int UP = 1;
+	final public static int RIGHT = 2;
+	final public static int DOWN = 3;
+	final public static int LEFT = 4;
+	
+	private Directions() {
+		// now cannot create instances
+	}
+}
